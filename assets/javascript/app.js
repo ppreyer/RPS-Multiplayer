d2366f5f201b3c40cd60451d36d0cdbe77a1abcb
ppreyer <parker.preyer1@gmail.com>
2017-10-16 10:20:57 -0400

    comments added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,52 +37,88 @@ var gameObject = {
         userTurn : 1
     }
 
+// Declare global variables
+// Counts user log-in number
 var userCounter = 0;
+// Firebase variable
 var database = firebase.database();
+// Stores firebase children for easy access
 var usersRef = database.ref().child('users');
 var playerOneRef = usersRef.child('players').child('one');
 var playerTwoRef = usersRef.child('players').child('two');
 
 // Store user log-in info
 $('#submit').on('click', function(){
+  // Prevent from submitting on button click
   event.preventDefault();
+  // Assign log-in value to object property
   gameObject.players.one.name = $('#log-in').val().trim();
+  // Add one to userCounter
   userCounter++;
+  // Conditional -- if first person to log in...
   if(userCounter === 1) {
-    var playerOne = gameObject;
-    usersRef.set(playerOne);
+    // Set firebase database to object
+    usersRef.set(gameObject);
+    // Delete playerOne div in html
     $('#playerOne p').empty();
+    // Declare playerTwo variable
     var playerTwo = gameObject.players.two;
-    database.ref().push(gameObject.userTurn);
+    // Look at first player values in firebase...
     playerOneRef.on('value', function(user){
+    // Declare first player name with username from firebase
     var userOneName = user.val().name;
+    // Append name to playerOne div in html
     $('#playerOne').append(userOneName);      
 })
+    // Else if player log-in === 2...
   } else if(userCounter === 2) {
-      // push player two info into firebase
+      // Declare player two name with second player log-in value
       var playerTwoName = gameObject.players.two.name;
       playerTwoName = $('#log-in').val().trim();
+      // Empty player two html div
       $('#playerTwo p').empty();
+      // Update firebase database with player two info
       playerTwoRef.update({
       'name' : playerTwoName
       });
+      // Look at player two data in firebase
       playerTwoRef.on('value', function(user){
+      // Assign a value to user two variable
       var userTwoName = user.val().name;
+      // Prepend data to user two div in html
       $('#playerTwo').prepend(userTwoName);
       })
+      // Invoke RPS function - defined below...
       renderRockPaperScissorsUserOne();
+      // Look at player one values in firebase
       playerOneRef.on('value', function(user){
-      var userOneWins = user.val().wins;
-      var userOneLosses = user.val().losses;
-      $('#playerOne').append('Wins:' + ' ' + userOneWins + ' ' + 'Losses:' + ' ' + userOneLosses);      
+        // Declare wins and losses variables
+        var userOneWins = user.val().wins;
+        var userOneLosses = user.val().losses;
+        // Append data to player one div in html
+        $('#playerOne').append('Wins:' + ' ' + userOneWins + ' ' + 'Losses:' + ' ' + userOneLosses);
       })
+      // Look at player two data in firebase
+      playerTwoRef.on('value', function(user){
+        // Declare wins and loss data from firebase
+        var userTwoWins = user.val().wins;
+        var userTwoLosses = user.val().losses;
+        // Append data to html
+        $('#playerTwo').append('<div id="rps"></div>')
+        $('#playerTwo').append('Wins:' + ' ' + userTwoWins + ' ' + 'Losses:' + ' ' + userTwoLosses);      
+      })
+      // else...
     } else {
+      // Tell the next user to wait their turn
       alert('Grab a beer and wait your turn!')
       }
 })
 
+// Function appends the appropriate game choices for user one
 function renderRockPaperScissorsUserOne() {
+  // Declare empty html variable
   var html = '';
+  // Add list of RPS choices
   html += '<div class="sectionOne">'
   html += '<ul class="gameChoices">';
   html += '<li class="choiceOne" value="rock">Rock</li>';
@@ -90,9 +126,11 @@ function renderRockPaperScissorsUserOne() {
   html += '<li class="choiceOne" value="scissors">Scissors</li>';
   html += '</ul>';
   html += '</div>'
+  // Append html to the page
   $('#playerOne').append(html);
 }
 
+// Function appends appropriate game choices for user two
 function renderRockPaperScissorsUserTwo() {
   var html = '';
   html += '<div class="sectionTwo">'
@@ -102,33 +140,29 @@ function renderRockPaperScissorsUserTwo() {
   html += '<li class="choiceTwo" value="scissors">Scissors</li>';
   html += '</ul>';
   html += '</div>'
-  $('#playerTwo').append(html);
+  $('#rps').append(html);
 }
-// function renderRockPaperScissorsUserTwo() {
-//   var html = '';
-//   html += '<div class="section">'
-//   html += '<ul class="gameChoices">';
-//   html += '<li class="choice" value="rock">Rock</li>';
-//   html += '<li class="choice" value="paper">Paper</li>';
-//   html += '<li class="choice" value="scissors">Scissors</li>';
-//   html += '</ul>';
-//   html += '</div>'
-//   $('#playerOne').append(html);
-// }
 
+// When user one clicks from the first set of choices...
 $(document).on('click', '.choiceOne', function(){
   console.log('CLICKED');
+  // Update firebase turn number in database
   usersRef.update({
     'userTurn' : 2
   })
+  // Conditional - if the choice === 'rock'
   if($(this).attr('value') === 'rock') {
+    // Empty list and append rock
     $('.sectionOne').empty();
     $('.sectionOne').append('<p>Rock</p>');
+    // Update firebase with user's selection
     var rock = $(this).attr('value');
     playerOneRef.update({
       'selection' : rock
     });
+    // Invoke userTwoSelection - defined below...
     userTwoSelection();
+    // Repeat above steps with paper
   } else if($(this).attr('value') === 'paper') {
      $('.sectionOne').empty();
       $('.sectionOne').append('<p>Paper</p>');
@@ -137,6 +171,7 @@ $(document).on('click', '.choiceOne', function(){
       'selection' : paper
       });
       userTwoSelection();
+      // Repeat above steps with scissors
     } else {
       $('.sectionOne').empty();
       $('.sectionOne').append('<p>Scissors</p>');
@@ -146,8 +181,9 @@ $(document).on('click', '.choiceOne', function(){
       });
       userTwoSelection();
       }
-})
+});
 
+// Repeat above click event listener but with player two choices
 $(document).on('click', '.choiceTwo', function(){
   console.log('CLICKED');
   usersRef.update({
@@ -178,12 +214,16 @@ $(document).on('click', '.choiceTwo', function(){
       });
       compareUserSelections();
       }
-})
+});
 
+// Function to determine if it's player two's turn
 function userTwoSelection() {
+  // Look at firebase database
   usersRef.on('value', function(user){
+    // Conditional -- if it's user two's turn...
     if(user.val().userTurn === 2) {
       console.log(user.val().userTurn);
+      // Invoke function to render RPS choices for that user
       renderRockPaperScissorsUserTwo();
       // playerTwoRef.on('value', function(user){
       // var userTwoWins = user.val().wins;
@@ -193,21 +233,27 @@ function userTwoSelection() {
     })
   }
 
+// Function compares choices made by both players
 function compareUserSelections() {
+  // Declare variables to store selections made by both players
   var userOneChoice = $('.sectionOne p').text().toLowerCase();
   var userTwoChoice = $('.sectionTwo p').text().toLowerCase();
   console.log(userOneChoice);
   console.log(userTwoChoice);
+  // Conditional - if both choices match up...
   if(userOneChoice === userTwoChoice) {
+    // Add one to ties for both players
     gameObject.players.one.ties++;
     gameObject.players.two.ties++;
+    // Update records in firebase
     playerOneRef.update({
       'ties' : gameObject.players.one.ties
     })
     playerTwoRef.update({
       'ties' : gameObject.players.two.ties
     })
-  } else if(userOneChoice === 'rock' && userTwoChoice === 'aper') {
+    // Go through and compare RPS choices with each other and update wins/losses appropriately...
+  } else if(userOneChoice === 'rock' && userTwoChoice === 'paper') {
       gameObject.players.one.losses++;
       gameObject.players.two.wins++;
       playerOneRef.update({
